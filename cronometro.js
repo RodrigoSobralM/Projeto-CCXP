da6c5ba7756b6d51fcc6e34928676096f01ef52d
ciueiCjay <rodrigo_sobral.m@hotmail.com>
2023-01-04 09:58:19 -0300

    finalização projeto

--- cronometro.js
+++ cronometro.js
@@ -3,7 +3,7 @@ const hora = document.getElementById("hora");
 const minuto = document.getElementById("minuto");
 const segundo = document.getElementById("segundo");
 
-const lancamento = "24 dec 2023"
+const lancamento = "03 jul 2023"
 
 function countDown() {
   const dataLanc = new Date(lancamento)
--- main.js
+++ main.js
@@ -1,9 +1,44 @@
-function addRedBorder(id){
-    element = document.querySelector("#" + id);
-    element.style.border = "5px solid red"
+function highlightCard(selector) {
+    var element = document.querySelector(selector);
+    element.classList.toggle("card-highlight");
 }
 
-function removeRedBorder(id){
-    elemente = document.querySelector("#" + id);
-    element.style.border = "0"
+function addKeyBoardEventListener() {
+    
+    document.addEventListener('keydown', (event) => {
+        var ingresso1 = document.getElementById('quinta');
+        var ingresso2 = document.getElementById('sexta');
+        var ingresso3 = document.getElementById('sabado');
+        var ingresso4 = document.getElementById('domingo');
+
+        var code = event.code;
+        if(code == 'Numpad1'){
+            ingresso1.classList.toggle("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+
+        if(code == 'Numpad2') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.toggle("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+
+        if(code == 'Numpad3') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.toggle("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+
+        if(code == 'Numpad4') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.toggle("card-highlight");
+        }
+    });
 }
+addKeyBoardEventListener()
